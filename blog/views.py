6ed1bd6bd46da91a2f4d73933edab86d1264b3dd
SGKespace <55636018+SGKespace@users.noreply.github.com>
2023-04-06 12:23:04 +0400

    reduce db queries number + Count

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.db.models import Count
 from blog.models import Comment, Post, Tag
+
+
 def get_related_posts_count(tag):
     return tag.posts.count()
+
+
 def get_likes_count(post):
     return post.num_likes
+
+
 def serialize_post(post):
     return {
         'title': post.title,
@@ -17,6 +23,8 @@ def serialize_post(post):
         'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
+
+
 def serialize_post_optimized(post):
     return {
         'title': post.title,
@@ -26,22 +34,22 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.annotate(Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': Post.objects.filter(tags=tag).count(),
+        'posts_with_tag': tag.posts__count,
     }
 
 
 def index(request):
     most_popular_posts = Post.objects.popular().prefetch_related('author', 'tags')[:5].fetch_with_comments_count()
-
     fresh_posts = Post.objects.annotate(Count('comments')).order_by('published_at').prefetch_related('author', 'tags')
     most_fresh_posts = list(fresh_posts)[-5:]
-
     popular_tags = Tag.objects.popular()
     most_popular_tags = popular_tags[:5]
     context = {
@@ -52,6 +60,8 @@ def index(request):
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
+
+
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
     comments = Comment.objects.filter(post=post)
@@ -62,8 +72,11 @@ def post_detail(request, slug):
             'published_at': comment.published_at,
             'author': comment.author.username,
         })
+
     likes = post.likes.all()
-    related_tags = post.tags.all()
+
+    related_tags = post.tags.annotate(Count('posts'))
+
     serialized_post = {
         'title': post.title,
         'text': post.text,
@@ -87,6 +100,8 @@ def post_detail(request, slug):
         ],
     }
     return render(request, 'post-details.html', context)
+
+
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
     all_tags = Tag.objects.all()
@@ -103,6 +118,8 @@ def tag_filter(request, tag_title):
         ],
     }
     return render(request, 'posts-list.html', context)
+
+
 def contacts(request):
     # позже здесь будет код для статистики заходов на эту страницу
     # и для записи фидбека
